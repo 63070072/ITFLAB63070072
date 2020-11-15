68794bb007074fd258694eb4269e57e14672d28f
tanadon parosin <73661116+tanadonparosin@users.noreply.github.com>
2020-11-15 21:54:41 +0700

    Update show.php

--- show.php
+++ show.php
@@ -8,7 +8,7 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
-<body>
+<body background-color: green;>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'tanadon.mysql.database.azure.com', 'tanadonparosin@tanadon', 'Nexus404', 'itflab', 3306);
