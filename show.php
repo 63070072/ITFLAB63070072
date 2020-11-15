f48cea2966fea14cd7aa69e54c95ca76e4aca70b
tanadon parosin <73661116+tanadonparosin@users.noreply.github.com>
2020-11-15 21:59:39 +0700

    Update show.php

--- show.php
+++ show.php
@@ -1,14 +1,19 @@
 <html>
 <head>
+<style>
 <title>ITF Lab</title>
-<meta charset="utf-8">
+body { 
+  background: lightblue; 
+}
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  </style>
 </head>
-<body background: lightblue;>
+<body>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'tanadon.mysql.database.azure.com', 'tanadonparosin@tanadon', 'Nexus404', 'itflab', 3306);
