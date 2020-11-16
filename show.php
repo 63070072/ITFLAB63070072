da7d9ec2e6ed1877ce396fba7509f81db9366cb3
tanadon parosin <73661116+tanadonparosin@users.noreply.github.com>
2020-11-16 20:04:27 +0700

    Update show.php

--- show.php
+++ show.php
@@ -23,7 +23,7 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table class="table table-hover table-bordered" style="width:800px">
+<table class="table table-hover table-bordered table-center" style="width:800px">
   <tr class="table-primary" >
     <th>Name</th>
     <th>Comment</th>
