61ab14a6df85cd3e2962c9aed55eb9845dfbc585
tanadon parosin <73661116+tanadonparosin@users.noreply.github.com>
2020-11-19 00:14:32 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,4 +1,18 @@
-
+<html>
+<head>
+<title>ITF Lab</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  p.ex1 {
+    margin-top: 25px;
+  }
+  </style>
+</head>
+<body>
 <?php
 
 $conn = mysqli_init();
@@ -19,20 +33,16 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 if (mysqli_query($conn, $sql)) {
   
-    echo "<center>New record created successfully</center>";
+    echo "New record created successfully";
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
   
 mysqli_close($conn);
 ?>
-<html>
+
 <div class="container">
   <center><a href="show.php"><input type="button" value="Show" class="btn btn-info" class="ex1"></a></center>
 </div>
-<body>
-{
-  background-color: rgba(172, 207, 246, 1);
-}
 </body>
 </html>
