a28ce99d4639840e502a0681f51f84321baa5af2
tanadon parosin <73661116+tanadonparosin@users.noreply.github.com>
2020-11-19 15:10:29 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<title>ITF Lab</title>
+<title>ITF Lab insert</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
